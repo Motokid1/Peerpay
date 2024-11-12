cfc4b71940f00edded0a22ff6d6684e3cad20e6c
Motokid1 <rohith.ganesh111@gmail.com>
2024-11-12 18:14:04 +0530

    first commit

--- script.js
+++ script.js
@@ -39,7 +39,45 @@ const account2 = {
   locale: "en-US",
 };
 
-const accounts = [account1, account2];
+const account3 = {
+  owner: "Venkat Anand",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "INR",
+  locale: "en-IN",
+};
+
+const account4 = {
+  owner: "Dokuparthi Nilesh",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "INR",
+  locale: "en-IN",
+};
+
+const accounts = [account1, account2, account3, account4];
 
 // DOM Elements
 const labelWelcome = document.querySelector(".welcome");
